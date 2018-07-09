508a1996e6b09231cb7403f44900ae634308d7f2
mahadihasan1400 <mahadihasan1400@gmail.com>
2018-07-09 16:13:58 +0600

    1st commit

---
+++ app/classes/Login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahad
+ * Date: 7/9/2018
+ * Time: 4:02 PM
+ */
+
+namespace App\classes;
+
+
+class Login
+{
+  public function adminLoginCheck($data){
+      echo '<pre>';
+      print_r($data);
+  }
+}
